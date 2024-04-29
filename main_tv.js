aadf9325bf3fd53d499382294ab99af343b0d066
cutecactusbunny <105020497+cutecactusbunny@users.noreply.github.com>
2024-04-28 20:02:13 -0500

    Add files via upload

---
+++ main_tv.js
@@ -0,0 +1,26 @@
+function preload(){
+    img = loadImage("mliving room pic.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+img = "";
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#338ED4");
+    text("Meerkat", 45, 75);
+    noFill();
+    stroke("#338ED4")
+    rect(30, 60, 450, 350 );
+
+    fill("#D133D4");
+    text("Other Meerkat", 320, 120);
+    noFill();
+    stroke("#D133D4")
+    rect(300, 90, 270, 320 );
+
+}
